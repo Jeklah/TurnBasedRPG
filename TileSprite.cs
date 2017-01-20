8b69b35104dd194800c94f00a02beb6c6d5f1649
Jeklah <arthurbowers@gmail.com>
2017-01-20 14:05:05 +0000

    Initial commit.

---
+++ TileSprite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[Serializable]
+public class TileSprite {
+    public string Name;
+    public Sprite tileImage;
+    public Tiles tileType;
+
+    public TileSprite()
+    {
+        Name = "Unset";
+        tileImage = new Sprite();
+        tileType = Tiles.Unset;
+    }
+
+    public TileSprite(string name, Sprite image, Tiles tile)
+    {
+        Name = name;
+        tileImage = image;
+        tileType = tile;
+    }
+}
